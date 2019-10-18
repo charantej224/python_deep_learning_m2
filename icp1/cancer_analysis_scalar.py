9d713f49a2afd200dd7a222c2c8546b51ed47c05
charantej224 <charantej.career@gmail.com>
2019-10-18 17:37:55 -0500

    icp1: 2 commit

--- icp1/cancer_analysis_scalar.py
+++ icp1/cancer_analysis_scalar.py
@@ -10,6 +10,10 @@ warnings.simplefilter("ignore")
 # Load the train DataFrames using pandas
 dataset = pd.read_csv('BreastCancer.csv')
 
+from sklearn.preprocessing import StandardScaler
+sc =StandardScaler()
+
+
 # Fill missing or null values with mean column values in the train set
 import numpy as np
 
@@ -23,19 +27,17 @@ print(dataset.head())
 
 dataset=dataset.values
 
+dataset = sc.fit_transform(dataset)
 
 X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,2:32], dataset[:,1],
                                                     test_size=0.25, random_state=87)
 np.random.seed(155)
 my_first_nn = Sequential() # create model
 my_first_nn.add(Dense(10, input_dim=30, activation='relu'))# hidden layer
-my_first_nn.add(Dense(12, activation='softplus'))
-#my_first_nn.add(Dense(156, activation='sigmoid')) #added one laye
-#my_first_nn.add(Dense(7, activation='sigmoid')) #added another layer
+#my_first_nn.add(Dense(12, activation='softplus'))
 my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
 my_first_nn.compile(loss='binary_crossentropy', optimizer='adam')
 my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
                                      initial_epoch=0)
 print(my_first_nn.summary())
 print(my_first_nn.evaluate(X_test, Y_test, verbose=0))
-#score = accuracy_score(dataset.target, pr)
